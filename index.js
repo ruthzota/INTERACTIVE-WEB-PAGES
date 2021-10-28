0a93338179cae5932c08dea3ff286710eb3082aa
ruthfee zota <ruthfeezota@gmail.com>
2021-10-28 09:47:51 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log('Hello World')
+
+
+function addMovie(event) {
+    let inputField = document.querySelector()
+}
+
+const movie = document.createElement('li')
+const movieTitle.textContent = inputField('span')
+movie.appendChild(movieTitle)
+
+
